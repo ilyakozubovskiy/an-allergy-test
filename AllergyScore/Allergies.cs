d1000daf8a40897417ac3c809784750eef70f076
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-10-12 13:44:08 +0000

    Upload New File

---
+++ AllergyScore/Allergies.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AllergyScore
+{
+    /// <summary>
+    /// Encapsulate the information about allergy test score and its analysis.
+    /// </summary>
+    public class Allergies
+    {
+        private readonly int score;
+
+        /// <summary>
+        /// Initializes the allergies object on base of test score.
+        /// </summary>
+        /// <param name="score">The allergy test score.</param>
+        /// <exception cref="ArgumentException">Thrown when score is less than zero.</exception>
+        public Allergies(int score)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+        
+        /// <summary>
+        /// Determines on base on the allergy test score for the given person, whether or not they're allergic to a given allergen(s).
+        /// </summary>
+        /// <param name="allergens">Allergens.</param>
+        /// <returns>true if there is an allergy to this allergen, false otherwise.</returns>
+        public bool IsAllergicTo(Allergens allergens)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+
+        /// <summary>
+        /// Determines the full list of allergies of the person with given allergy test score.
+        /// </summary>
+        /// <returns>Full list of allergies of the person with given allergy test score.</returns>
+        public Allergens[] AllergensList()
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
